56ea2d3bfe6a98d900e33a2f1680755cf42e7777
Anastasiia Dosyn <anastasiia.dosyn@student.tuke.sk>
2024-11-29 20:20:19 +0100

    perf: update main

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-from elements.Board import Board
+from gui.controller import RushHourGame
+import tkinter as tk
 
 def run_game():
-    level_grid = Board.init_board('levels/level1.txt')
-    board = Board(level_grid)
-
-    print("Vehicles on the board:")
-    for vehicle_id, vehicle in board.cars.items():
-        print(f"Vehicle {vehicle_id}: {vehicle.positions}")
-
-    print("Game initialized!")
-
+    # level_grid = Board.init_board('levels/level1.txt')
+    # board = Board(level_grid)
+    #
+    # print("Vehicles on the board:")
+    # for vehicle_id, vehicle in board.cars.items():
+    #     print(f"Vehicle {vehicle_id}: {vehicle.positions}")
+    #
+    # print("Game initialized!")
+    root = tk.Tk()
+    RushHourGame(root, 'levels/level1.txt')
+    root.mainloop()
 
 
 if __name__ == '__main__':
