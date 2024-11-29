743af730344f1da867733e9037c8542c3f566a26
Anastasiia Dosyn <anastasiia.dosyn@student.tuke.sk>
2024-11-29 20:21:58 +0100

    perf: split gui class to interface and controller for better code management

---
+++ gui/controller.py
@@ -0,0 +1,36 @@
+from interface import RushHourGUI
+from ..elements.Board import Board
+
+
+class RushHourGame:
+    def __init__(self, root, level_file):
+        self.root = root
+        self.root.title("Rush Hour")
+
+        # Завантаження рівня
+        self.board = Board(Board.init_board(level_file))  # Використання класу Board
+
+        # GUI
+        self.gui = RushHourGUI(self.root, self.board, self.move_car)
+
+        # Зв'язування клавіш для руху
+        self.root.bind("<Up>", lambda event: self.move_car("up"))
+        self.root.bind("<Down>", lambda event: self.move_car("down"))
+        self.root.bind("<Left>", lambda event: self.move_car("left"))
+        self.root.bind("<Right>", lambda event: self.move_car("right"))
+
+    def move_car(self, direction):
+        if self.gui.selected_car is None:
+            print("No car selected!")
+            return
+
+        car = self.board.cars[self.gui.selected_car]
+        if not self.board.is_valid_move(car, direction):
+            print("Invalid move!")
+            return
+
+        # Оновлення позицій на дошці
+        self.board.move_car(car, direction)
+
+        # Оновлення GUI
+        self.gui.update_board()
