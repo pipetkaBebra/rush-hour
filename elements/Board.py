8e29153d3f41aa0d46552eb9dc5d3f093aebe7da
pipetkaBebra <shurikboy05@gmail.com>
2024-12-04 20:19:27 +0100

    Update Board.py

--- elements/Board.py
+++ elements/Board.py
@@ -1,14 +1,10 @@
 from .Car import Car
-
-
 class Board:
     def __init__(self, grid):
-        self.gui = None
         self.grid = grid
         self.cars = self._parse_cars()
-        self.grid_size = 6
+        self.grid_size = len(grid)
 
-    #метод для парсингу даних з файлу, запис позицій машинок
     def _parse_cars(self):
         cars = {}
         for row_idx, row in enumerate(self.grid):
@@ -21,7 +17,6 @@ class Board:
 
     def is_valid_move(self, car, direction):
         dx, dy = 0, 0
-        #рух машинок за допомогою стрілочок відносно орієнтації машинок на полі (якщо машинка знаходиться горизонтально на полі, то рухатися вона теж може тільки по горизонталі)
         if direction == "up" and car.orientation == "vertical":
             dx = -1
         elif direction == "down" and car.orientation == "vertical":
@@ -35,11 +30,9 @@ class Board:
 
         new_positions = [(x + dx, y + dy) for x, y in car.positions]
 
-        #перевірка на межі (чи не виходить машинка за межі)
         if not all(0 <= x < len(self.grid) and 0 <= y < len(self.grid[0]) for x, y in new_positions):
             return False
 
-        #перевірка на перешкоди (чи нема іншої машинки)
         for x, y in new_positions:
             if self.grid[x][y] != "." and self.grid[x][y] != car.name:
                 return False
@@ -57,22 +50,40 @@ class Board:
         elif direction == "right":
             dy = 1
 
-        #очищення старих позицій
         for x, y in car.positions:
             self.grid[x][y] = "."
 
-        #оновлення позицій
         car.positions = [(x + dx, y + dy) for x, y in car.positions]
 
-        #запис нових позицій
         for x, y in car.positions:
             self.grid[x][y] = car.name
 
     @staticmethod
-    #зчитування даних з файлу
     def init_board(file):
         grid = []
         with open(file, 'r') as f:
             for line in f:
                 grid.append(line.strip().split())
         return grid
+
+    def copy(self):
+        new_board = Board([row[:] for row in self.grid])
+        new_board.cars = {car_name: Car(car_name) for car_name in self.cars}
+        for car_name, car in self.cars.items():
+            new_car = new_board.cars[car_name]
+            new_car.positions = car.positions[:]
+            new_car.orientation = car.orientation
+        return new_board
+
+    def board_to_key(self):
+        return ''.join(''.join(row) for row in self.grid)
+
+    def __hash__(self):
+        """Генерує хеш для стану дошки."""
+        return hash(tuple(tuple(row) for row in self.grid))  # Унікальне представлення стану
+
+    def __eq__(self, other):
+        """Перевіряє еквівалентність двох станів."""
+        if not isinstance(other, Board):
+            return False
+        return self.grid == other.grid
